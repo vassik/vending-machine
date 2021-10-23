e8f2602d84208c0eea25bc1a482b07b71fde1bb6
vassik <anvasilevsky@gmail.com>
2021-10-23 21:49:00 +0200

    better message to console

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ localhost:8080/swagger-ui.html
 http://localhost:8080/api-docs/
 
 http://localhost:8080/api-docs.yaml
+
+
+./mvnw spring-boot:build-image -Dspring-boot.build-image.imageName=vassik/rvm
+
+
--- src/main/java/org/rvm/dto/Receipt.java
+++ src/main/java/org/rvm/dto/Receipt.java
@@ -13,6 +13,16 @@ public class Receipt {
         this.total = 0;
     }
 
+    @Override
+    public String toString() {
+        String stringReceipt = "";
+        for (Container.Type key : containers.keySet()) {
+            stringReceipt += String.format("%s X %d,", key, containers.get(key));
+        }
+        stringReceipt += String.format("total: %d NOK", total);
+        return stringReceipt;
+    }
+
     public Receipt(Map<Container.Type, Integer> containers, Integer total) {
         this.containers = containers;
         this.total = total;
